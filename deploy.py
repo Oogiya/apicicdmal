bd52e46100caef28b1659ab755ff1721f0be9b55
Miyu Levin <4evermiyu@gmail.com>
2020-03-13 22:59:02 +0200

    auto heroku test

--- deploy.py
+++ deploy.py
@@ -26,4 +26,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>' + GetAnimeByName("Made in Abyss") + '</h1>'
+    return '<h1>' + GetAnimeByName("fullmetal alchemist") + '</h1>'
